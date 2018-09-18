99ad543772917ae7e468a4c927bb489bbdb2ea82
yen <yen@debian>
2018-09-19 01:52:17 +0800

    up: fix v4, v7

--- app/src/main/java/com/flask/colorpicker/sample/SampleActivity3.java
+++ app/src/main/java/com/flask/colorpicker/sample/SampleActivity3.java
@@ -1,18 +1,19 @@
 package com.flask.colorpicker.sample;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class SampleActivity3 extends AppCompatActivity {
 
